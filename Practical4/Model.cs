c2f326297d85cec77e63c4fc4fdf11d8e2d24ef4
沙耶 <yhsia@codersai.com>
2022-04-01 08:33:21 +0800

    Add project files.

---
+++ Practical4/Model.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical4
+{
+    public class ClassA
+    {
+        public virtual void PrintMethod()
+        {
+            Console.WriteLine("Print from Class A");
+        }
+
+        public void SecondPrintMethod()
+        {
+            Console.WriteLine("Second print from Class A");
+        }
+
+        //method overloading with optional parameter
+        public void PrintMethod(string name,int id=0)
+        {
+
+        }
+    }
+
+    public class ClassB : ClassA
+    {
+        //overriding
+        public override void PrintMethod()
+        {
+            Console.WriteLine("Print from Class B");
+        }
+
+        //shadowing
+        public new void SecondPrintMethod()
+        {
+            Console.WriteLine("Second Print from Class B");
+        }
+    }
+
+}
---
+++ Practical4/Program.cs
@@ -0,0 +1,16 @@
+﻿using Practical4;
+
+//ClassA objA = new ClassA();
+//objA.PrintMethod("Name");
+//ClassB objB = new ClassB();
+//objB.PrintMethod();
+
+
+ClassA objA = new ClassB();
+objA.PrintMethod();
+objA.SecondPrintMethod();
+//ClassB objB = new ClassB();
+
+Console.ReadLine();
+
+
